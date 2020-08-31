e567026bd1173c997e9542d6303db2c4e9e7c726
Ian Hunter <ian@ianhunter.me>
2020-08-31 15:21:30 -0400

    Update version in mix project file

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WindexPlug.MixProject do
   def project do
     [
       app: :windex_plug,
-      version: "0.1.0",
+      version: "1.1.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
