c342c0f9b732f7ff7c0eea5f78e4f105fa25850b
Ian Hunter <ian@ianhunter.me>
2020-09-04 11:34:06 -0400

    Fix compilation

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WindexPlug.MixProject do
   def project do
     [
       app: :windex_plug,
-      version: "1.2.4",
+      version: "1.2.5",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -14,6 +14,7 @@ defmodule WindexPlug.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:plug, :windex],
       extra_applications: [:logger]
     ]
   end
@@ -21,7 +22,7 @@ defmodule WindexPlug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:windex, git: "git@github.com:VidAngel/windex.git", tag: "0.3.5"},
+      {:windex, git: "git@github.com:VidAngel/windex.git", tag: "0.3.6"},
       {:plug, "~> 1.10.3"},
       {:jason, "~> 1.2.1"},
     ]
